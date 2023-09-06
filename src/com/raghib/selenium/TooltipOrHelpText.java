69a614a6f9ebae59df5ea391df171f00e47a8c29
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:46:25 +0530

    Converted into maven project.

--- src/com/raghib/selenium/TooltipOrHelpText.java
+++ src/com/raghib/selenium/TooltipOrHelpText.java
@@ -1,26 +1,32 @@
 package com.raghib.selenium;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class TooltipOrHelpText {
+public class TooltipOrHelpText extends BaseClass {
 
+	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
+	
 	// Mouse Over
 	public static String tooltipHeader = "//a[text()='Tooltip']";
 	// URL
-	public static String bootstrapTooltipURL = System.getProperty("user.dir")+"\\SupportFile\\Tooltips.html";
+	public static String bootstrapTooltipURL = System.getProperty("user.dir") + "\\SupportFile\\Tooltips.html";
 	// Capture the text and store in a variable and validate the same.
 	public static String TooltipMessage = "//div[@class='tooltip-inner']";
-
+	
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
-		driver.get(bootstrapTooltipURL);
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
+		driver.get(bootstrapTooltipURL);
+		
 		WebElement moveToElement = driver.findElement(By.xpath(tooltipHeader));
 
 		Actions action = new Actions(driver);
@@ -30,6 +36,6 @@ public class TooltipOrHelpText {
 
 		System.out.println("Result : " + result);
 
-		//driver.quit();
+		//BaseClass.quitDriver();
 	}
 }
